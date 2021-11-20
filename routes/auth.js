171419312dce2208945b334793814ef285535a78
Domingos Creado <domingos.creado@gmail.com>
2021-11-20 09:56:06 -0300

    including the https init and removing the goto parameter

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/login', function(req, res, next) {
-  res.render('login', { query: req.query});
+  res.render('login');
 });
 
 router.post('/login/password', function(req, res, next) {
-  var redirect_back = req.body["goto"] ;
+  var redirect_back = req.session.returnTo;
 
   if(redirect_back == undefined || redirect_back == "" || !(redirect_back.startsWith("/"))) {
     redirect_back = "/";
@@ -17,7 +17,7 @@ router.post('/login/password', function(req, res, next) {
 
   passport.authenticate('local', function(err, user, info) {
     if (err) { return next(err); }
-    if (!user) { return res.redirect('/login?goto='+ encodeURIComponent(redirect_back)); }
+    if (!user) { return res.redirect('/login'); }
     req.logIn(user, function(err) {
       if (err) { return next(err); }
       return res.redirect(redirect_back);
